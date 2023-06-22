248d57202988f1f5de54cb371468b978af7fecad
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-22 12:06:38 +0300

    not fully done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, name){
+    return drivers.filter(name => name  == "Bobby" && "bobby")
+}
+findMatching(drivers, 'Bobby')
